643c28e9ef8a3713f5cc360d5080eb914e306e8e
FeyisaraO <feyisaraodukoya@gmail.com>
2023-07-12 16:57:46 +0100

    Initial Commit

---
+++ instagram/instagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramApp.swift
+//  instagram
+//
+//  Created by Feyisara Odukoya on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct instagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
